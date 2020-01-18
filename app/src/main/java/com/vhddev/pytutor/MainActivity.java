54af23b595337400bc49c3ada9f9d31edf11db7b
suhu3515 <suhu3515@gmail.com>
2020-01-18 21:09:41 +0530

    DAY 2 =======
    ==> SPLASH SCREEN DONE.

--- app/src/main/java/com/vhddev/pytutor/MainActivity.java
+++ app/src/main/java/com/vhddev/pytutor/MainActivity.java
@@ -2,13 +2,27 @@ package com.vhddev.pytutor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int spl_time = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent proIntent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(proIntent);
+
+            }
+        },spl_time);
     }
 }
